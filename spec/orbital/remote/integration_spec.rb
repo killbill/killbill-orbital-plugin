31dfe2fc7942cb5f6604e143eeb4b648ffdf9f2e
Dali <dwei@groupon.com>
2016-11-22 17:14:09 -0800

    Add specs for purchase regarding cvv indicator

--- spec/orbital/remote/integration_spec.rb
+++ spec/orbital/remote/integration_spec.rb
@@ -102,10 +102,6 @@ shared_examples 'common_specs' do
 end
 
 shared_examples 'cvv_indicator_specs' do
-  before(:each) do
-    create_payment
-  end
-
   after(:each) do
     @plugin.stop_plugin
   end
@@ -114,7 +110,6 @@ shared_examples 'cvv_indicator_specs' do
     # The default card type is visa
     validate_cvv_indicator_field 1
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'discover' })
     validate_cvv_indicator_field 1
 
@@ -122,7 +117,6 @@ shared_examples 'cvv_indicator_specs' do
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cvv_indicator_visa_discover => true })
     validate_cvv_indicator_field 1
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'discover', :cvv_indicator_visa_discover => true  })
     validate_cvv_indicator_field 1
   end
@@ -132,7 +126,6 @@ shared_examples 'cvv_indicator_specs' do
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field 9
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'discover', :cvv_indicator_visa_discover => true })
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field 9
@@ -143,7 +136,6 @@ shared_examples 'cvv_indicator_specs' do
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'discover'})
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
@@ -152,7 +144,6 @@ shared_examples 'cvv_indicator_specs' do
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'discover', :cvv_indicator_visa_discover => false })
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
@@ -162,14 +153,18 @@ shared_examples 'cvv_indicator_specs' do
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'master' })
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express' })
+    validate_cvv_indicator_field
+
+    @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'master', :cvv_indicator_visa_discover => false })
+    validate_cvv_indicator_field
+
+    @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express', :cvv_indicator_visa_discover => false })
     validate_cvv_indicator_field
 
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'master', :cvv_indicator_visa_discover => true})
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express', :cvv_indicator_visa_discover => true })
     validate_cvv_indicator_field
 
@@ -177,8 +172,15 @@ shared_examples 'cvv_indicator_specs' do
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express' })
+    @properties.reject! {|property| property.key == 'ccVerificationValue' }
+    validate_cvv_indicator_field
+
+    @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'master', :cvv_indicator_visa_discover => false})
+    @properties.reject! {|property| property.key == 'ccVerificationValue' }
+    validate_cvv_indicator_field
+
+    @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express', :cvv_indicator_visa_discover => false })
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
 
@@ -186,7 +188,6 @@ shared_examples 'cvv_indicator_specs' do
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
 
-    create_payment
     @properties = build_pm_properties(nil, { :cc_number => '5454545454545454', :cc_type => 'american_express', :cvv_indicator_visa_discover => true })
     @properties.reject! {|property| property.key == 'ccVerificationValue' }
     validate_cvv_indicator_field
@@ -245,7 +246,13 @@ describe Killbill::Orbital::PaymentPlugin do
 
   def successful_authorize_response
     <<-XML
-<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><NewOrderResp><IndustryType></IndustryType><MessageType>A</MessageType><MerchantID>1111111</MerchantID><TerminalID>001</TerminalID><CardBrand>MC</CardBrand><AccountNum>XXXXXXXXXXXX5454</AccountNum><OrderID>5b257b31-1f84-44bc-b32</OrderID><TxRefNum>5834AA75E4466AEA59512165057C37DD810053C2</TxRefNum><TxRefIdx>0</TxRefIdx><ProcStatus>0</ProcStatus><ApprovalStatus>1</ApprovalStatus><RespCode>00</RespCode><AVSRespCode>B </AVSRespCode><CVV2RespCode> </CVV2RespCode><AuthCode>tst424</AuthCode><RecurringAdviceCd></RecurringAdviceCd><CAVVRespCode></CAVVRespCode><StatusMsg>Approved</StatusMsg><RespMsg></RespMsg><HostRespCode>100</HostRespCode><HostAVSRespCode>I3</HostAVSRespCode><HostCVV2RespCode>  </HostCVV2RespCode><CustomerRefNum></CustomerRefNum><CustomerName></CustomerName><ProfileProcStatus></ProfileProcStatus><CustomerProfileMessage></CustomerProfileMessage><RespTime>152837</RespTime><PartialAuthOccurred></PartialAuthOccurred><RequestedAmount></RequestedAmount><RedeemedAmount></RedeemedAmount><RemainingBalance></RemainingBalance><CountryFraudFilterStatus></CountryFraudFilterStatus><IsoCountryCode></IsoCountryCode></NewOrderResp></Response>
+<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><NewOrderResp><IndustryType></IndustryType><MessageType>A</MessageType><MerchantID>1111111</MerchantID><TerminalID>001</TerminalID><CardBrand>CC</CardBrand><AccountNum>XXXXXXXXXXXX5454</AccountNum><OrderID>5b257b31-1f84-44bc-b32</OrderID><TxRefNum>5834AA75E4466AEA59512165057C37DD810053C2</TxRefNum><TxRefIdx>0</TxRefIdx><ProcStatus>0</ProcStatus><ApprovalStatus>1</ApprovalStatus><RespCode>00</RespCode><AVSRespCode>B </AVSRespCode><CVV2RespCode> </CVV2RespCode><AuthCode>tst424</AuthCode><RecurringAdviceCd></RecurringAdviceCd><CAVVRespCode></CAVVRespCode><StatusMsg>Approved</StatusMsg><RespMsg></RespMsg><HostRespCode>100</HostRespCode><HostAVSRespCode>I3</HostAVSRespCode><HostCVV2RespCode>  </HostCVV2RespCode><CustomerRefNum></CustomerRefNum><CustomerName></CustomerName><ProfileProcStatus></ProfileProcStatus><CustomerProfileMessage></CustomerProfileMessage><RespTime>152837</RespTime><PartialAuthOccurred></PartialAuthOccurred><RequestedAmount></RequestedAmount><RedeemedAmount></RedeemedAmount><RemainingBalance></RemainingBalance><CountryFraudFilterStatus></CountryFraudFilterStatus><IsoCountryCode></IsoCountryCode></NewOrderResp></Response>
+    XML
+  end
+
+  def successful_purchase_response
+    <<-XML
+<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><NewOrderResp><IndustryType></IndustryType><MessageType>AC</MessageType><MerchantID>1111111</MerchantID><TerminalID>001</TerminalID><CardBrand>CC</CardBrand><AccountNum>XXXXXXXXXXXX5454</AccountNum><OrderID>88132d30-f4f7-4028-949</OrderID><TxRefNum>5834EAC9C7A53FEB600A479629FB6C6427A2532C</TxRefNum><TxRefIdx>1</TxRefIdx><ProcStatus>0</ProcStatus><ApprovalStatus>1</ApprovalStatus><RespCode>00</RespCode><AVSRespCode>3 </AVSRespCode><CVV2RespCode>I</CVV2RespCode><AuthCode>tst703</AuthCode><RecurringAdviceCd></RecurringAdviceCd><CAVVRespCode></CAVVRespCode><StatusMsg>Approved</StatusMsg><RespMsg></RespMsg><HostRespCode>100</HostRespCode><HostAVSRespCode>  </HostAVSRespCode><HostCVV2RespCode>I</HostCVV2RespCode><CustomerRefNum></CustomerRefNum><CustomerName></CustomerName><ProfileProcStatus></ProfileProcStatus><CustomerProfileMessage></CustomerProfileMessage><RespTime>200306</RespTime><PartialAuthOccurred></PartialAuthOccurred><RequestedAmount></RequestedAmount><RedeemedAmount></RedeemedAmount><RemainingBalance></RemainingBalance><CountryFraudFilterStatus></CountryFraudFilterStatus><IsoCountryCode></IsoCountryCode></NewOrderResp></Response>    XML
     XML
   end
 
@@ -256,12 +263,23 @@ describe Killbill::Orbital::PaymentPlugin do
       else
         request_body.should match("<CardSecValInd>#{expected_field}</CardSecValInd>")
       end
-      successful_authorize_response
+      if request_body.include? '<MessageType>A</MessageType>'
+        successful_authorize_response
+      else
+        successful_purchase_response
+      end
     end
+    create_payment
     payment_response = @plugin.authorize_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
     payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
     payment_response.transaction_type.should == :AUTHORIZE
+
+    create_payment
+    payment_response = @plugin.purchase_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
+    payment_response.amount.should == @amount
+    payment_response.transaction_type.should == :PURCHASE
   end
 
   def create_payment
