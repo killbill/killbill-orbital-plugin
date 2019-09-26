848b3f5fff6c214164c656b2e69102f9b364dc8a
Ankita Ghosh <ankghosh@groupon.com>
2019-09-26 15:37:20 +0530

    logs removed KIBI-1155

--- lib/orbital/api.rb
+++ lib/orbital/api.rb
@@ -74,10 +74,8 @@ module Killbill #:nodoc:
       end
 
       def refund_payment(kb_account_id, kb_payment_id, kb_payment_transaction_id, kb_payment_method_id, amount, currency, properties, context)
-        logger.info("Inside refund payment'#{properties}'")
         credit_opts = properties_to_hash(properties)
         if should_credit?(kb_payment_id, context, credit_opts)
-          logger.info("Inside should credit '#{credit_opts}'")
           # Note: from the plugin perspective, this transaction is a CREDIT but Kill Bill doesn't care about PaymentTransactionInfoPlugin#TransactionType
           return credit_payment(kb_account_id, kb_payment_id, kb_payment_transaction_id, kb_payment_method_id, amount, currency, hash_to_properties(credit_opts), context)
         end
@@ -153,7 +151,6 @@ module Killbill #:nodoc:
       end
 
       def should_credit?(kb_payment_id, context, options = {})
-        logger.info("Inside should credit method '#{options}'")
         transaction = @transaction_model.find_candidate_transaction_for_refund(kb_payment_id, context.tenant_id)
         return false if transaction.nil?
 
